8520529c044fd36cc0d6dd87a4b1d860e0b7d400
Klortiz <klortiz@ucsd.edu>
2023-01-25 16:26:45 -0800

    Hi
    Hi

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,27 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {13, 56, 75, 42};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{42, 75, 56, 13}, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {13,75,18, 35};
+    assertArrayEquals(new int[]{35,18,75,13}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void findAverageWithoutLowest() {
+    double[] numbers = {15.0,20.0,25.0,60.0,40.0};
+    assertEquals(36.25, ArrayExamples.averageWithoutLowest(numbers),36.25);
+  }
+
+  @Test
+  public void AvgWithoutLowest() {
+    double[] numbers = {1,2,3,4,5,6,0};
+    assertEquals(3.5, ArrayExamples.averageWithoutLowest(numbers),0.1);
   }
 }
---
+++ ListTests.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+import org.junit.*;
+
+public class ListTests {
+    @Test
+    public void checkString() {
+        StringChecker scheck = new StringChecker() {
+            public boolean checkString(String s) {
+                if(s != null) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        };
+        ArrayList<String> check = new ArrayList<>();
+        check.add("hi");
+        check.add(null);
+        check.add("hello");
+        ArrayList<Boolean> b = new ArrayList<>();
+        b.add(true);
+        b.add(false);
+        ArrayList<String> a = new ArrayList<>();
+        a.add(0,"hi");
+        a.add(0,"hello");
+        assertEquals(a, ListExamples.filter(check, scheck));
+
+        
+    }
+
+
+
+
+}
